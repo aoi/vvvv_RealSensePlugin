1f48fe9faffb9ee1d67e178531d3cce6db559822
Satoshi ETO <japanpower1987@gmail.com>
2016-01-03 07:41:49 +0900

    SegmentationNode.cs、BaseNode.csを作成.

---
+++ plugins/RealSense.Nodes/BaseNode.cs
@@ -0,0 +1,240 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+using VVVV.DX11;
+using VVVV.Utils.VMath;
+
+using SlimDX.Direct3D11;
+
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+
+using VVVV.Core.Logging;
+#endregion
+
+namespace RealSense.Nodes
+{
+    public abstract class BaseNode : IPluginEvaluate, IDX11ResourceProvider, IDisposable
+    {
+
+        protected const int WIDTH = 640;
+        protected const int HEIGHT = 480;
+        protected const int FPS = 30;
+        protected const int BYTE_PER_PIXEL = 4;
+
+        [Input("Apply", IsSingle = true, DefaultValue = 1)]
+        protected ISpread<bool> FApply;
+
+        [Input("Enabled", IsSingle = true, DefaultValue = 0)]
+        protected ISpread<bool> FInEnabled;
+
+        [Output("Texture Out")]
+        protected Pin<DX11Resource<DX11DynamicTexture2D>> FTextureOutput;
+
+        protected bool initialized = false;
+        protected bool FInvalidate = false;
+
+        protected PXCMSession session;
+        protected PXCMSenseManager senseManager;
+        protected PXCMCapture.Device device;
+
+        protected PXCMImage image;
+
+        [Import()]
+        protected ILogger FLogger;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.initialized && !FInEnabled[0])
+            {
+                this.Uninitialize();
+            }
+
+            if (!FInEnabled[0]) { return; }
+
+            if (this.FApply[0]) { this.FInvalidate = true; }
+
+            if (this.image == null)
+            {
+                if (this.FTextureOutput.SliceCount == 1)
+                {
+                    if (this.FTextureOutput[0] != null) { this.FTextureOutput[0].Dispose(); }
+                    this.FTextureOutput.SliceCount = 0;
+                }
+            }
+            else
+            {
+                this.FTextureOutput.SliceCount = 1;
+                if (this.FTextureOutput[0] == null) { this.FTextureOutput[0] = new DX11Resource<DX11DynamicTexture2D>(); }
+            }
+
+            if (!initialized)
+            {
+                try
+                {
+                    this.Initialize();
+                }
+                catch (Exception e)
+                {
+                    FLogger.Log(LogType.Error, e.Message + e.StackTrace);
+
+                    this.Uninitialize();
+                }
+            }
+            else
+            {
+                try
+                {
+                    this.UpdateFrame();
+                }
+                catch (Exception e)
+                {
+                    FLogger.Log(LogType.Error, "UpdateFrame: " + e.Message + e.StackTrace);
+                    this.Uninitialize();
+                }
+            }
+        }
+
+        protected abstract void Initialize();
+
+        protected virtual void Uninitialize()
+        {
+            if (this.image != null)
+            {
+                this.image.Dispose();
+            }
+
+            if (this.device != null)
+            {
+                this.device.Dispose();
+                this.device = null;
+            }
+
+            if (this.senseManager != null)
+            {
+                this.senseManager.Close();
+                this.senseManager.Dispose();
+                this.senseManager = null;
+            }
+
+            if (this.session != null)
+            {
+                this.session.Dispose();
+                this.session = null;
+            }
+
+            this.initialized = false;
+        }
+
+        protected abstract void UpdateFrame();
+
+        protected abstract byte[] GetImageBuffer();
+
+        protected void EnableColorStream()
+        {
+            pxcmStatus sts = this.senseManager.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, WIDTH, HEIGHT, FPS);
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("カラーストリームの有効化に失敗しました");
+            }
+        }
+
+        protected void EnableDepthStream()
+        {
+            pxcmStatus sts = this.senseManager.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_DEPTH, WIDTH, HEIGHT, FPS);
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("Depthストリームの有効化に失敗しました");
+            }
+        }
+
+        protected void SenseManagerInit()
+        {
+            pxcmStatus sts = this.senseManager.Init();
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("初期化に失敗しました: " + sts.ToString());
+            }
+        }
+
+        protected void SetMirrorMode()
+        {
+            pxcmStatus sts = this.device.SetMirrorMode(PXCMCapture.Device.MirrorMode.MIRROR_MODE_HORIZONTAL);
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("ミラー表示の設定に失敗しました");
+            }
+        }
+
+        public void Update(IPluginIO pin, DX11RenderContext context)
+        {
+            if (!this.initialized) { return; }
+
+            if (this.FTextureOutput.SliceCount == 0) { return; }
+
+            if (this.FInvalidate || !this.FTextureOutput[0].Contains(context))
+            {
+
+                SlimDX.DXGI.Format fmt = SlimDX.DXGI.Format.B8G8R8A8_UNorm;
+
+                Texture2DDescription desc;
+
+                if (this.FTextureOutput[0].Contains(context))
+                {
+                    desc = this.FTextureOutput[0][context].Resource.Description;
+
+                    if (desc.Width != WIDTH || desc.Height != HEIGHT || desc.Format != fmt)
+                    {
+                        this.FTextureOutput[0].Dispose(context);
+                        DX11DynamicTexture2D t2D = new DX11DynamicTexture2D(context, WIDTH, HEIGHT, fmt);
+
+                        this.FTextureOutput[0][context] = t2D;
+                    }
+                }
+                else
+                {
+                    this.FTextureOutput[0][context] = new DX11DynamicTexture2D(context, WIDTH, HEIGHT, fmt);
+#if DEBUG
+                    this.FTextureOutput[0][context].Resource.DebugName = "DynamicTexture";
+#endif
+                }
+
+                desc = this.FTextureOutput[0][context].Resource.Description;
+
+                var t = this.FTextureOutput[0][context];
+
+                var buffer = this.GetImageBuffer();
+                if (buffer != null)
+                {
+                    t.WriteData(buffer);
+                    this.FInvalidate = false;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            FLogger.Log(LogType.Debug, "Dispose");
+            if (this.FTextureOutput.SliceCount > 0)
+            {
+                if (this.FTextureOutput[0] != null)
+                {
+                    this.FTextureOutput[0].Dispose();
+                }
+            }
+
+            this.Uninitialize();
+
+        }
+
+        public void Destroy(IPluginIO pin, DX11RenderContext context, bool force)
+        {
+            FLogger.Log(LogType.Debug, "Destory");
+            this.FTextureOutput[0].Dispose(context);
+        }
+    }
+}
---
+++ plugins/RealSense.Nodes/SegmentationNode.cs
@@ -0,0 +1,165 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+using VVVV.DX11;
+using VVVV.Utils.VMath;
+
+using SlimDX.Direct3D11;
+
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+
+using VVVV.Core.Logging;
+#endregion
+
+namespace RealSense.Nodes
+{
+    [PluginInfo(Name = "Segmentation", Category = "RealSense", Version = "Intel", Help = "RealSense Segmentation Image.", Tags = "RealSense, DX11, texture", Author = "aoi")]
+    public class SegmentationNode : BaseNode
+    {
+
+        private PXCMSession segSession;
+        private PXCM3DSeg segmentation;
+
+        protected override void Initialize()
+        {
+            this.session = PXCMSession.CreateInstance();
+            this.segSession = PXCMSession.CreateInstance();
+            if (this.session == null || this.segSession == null)
+            {
+                throw new Exception("セッションを作成できませんでした");
+            }
+            FLogger.Log(LogType.Debug, this.session.QueryVersion().major + ":" + this.session.QueryVersion().minor);
+            this.senseManager = this.session.CreateSenseManager();
+            if (this.senseManager == null)
+            {
+                throw new Exception("マネージャを作成できませんでした");
+            }
+
+            this.EnableColorStream();
+            //this.EnableDepthStream();
+            pxcmStatus sts = this.senseManager.Enable3DSeg();
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("セグメンテーションの有効化に失敗しました");
+            }
+
+            // パイプラインを初期化する
+            this.SenseManagerInit();
+
+            this.segmentation = this.senseManager.Query3DSeg();
+            if (this.segmentation == null)
+            {
+                throw new Exception("セグメンテーションの取得に失敗しました");
+            }
+
+            this.device = this.senseManager.QueryCaptureManager().QueryDevice();
+            if (this.device == null)
+            {
+                throw new Exception("デバイスを取得できませんでした");
+            }
+
+            this.SetMirrorMode();
+            sts = this.device.SetMirrorMode(PXCMCapture.Device.MirrorMode.MIRROR_MODE_HORIZONTAL);
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                throw new Exception("ミラー表示の設定に失敗しました");
+            }
+
+            this.initialized = true;
+        }
+
+        protected override void UpdateFrame()
+        {
+            // フレームを取得する
+            pxcmStatus sts = this.senseManager.AcquireFrame(false);
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                return;
+            }
+
+            if (this.segmentation != null)
+            {
+                this.image = this.segmentation.AcquireSegmentedImage();
+            }
+            else
+            {
+                this.image = this.senseManager.QuerySample().color;
+            }
+
+            // フレームを開放する
+            this.senseManager.ReleaseFrame();
+        }
+
+        protected override byte[] GetImageBuffer()
+        {
+            if (this.image == null) { return null; }
+
+            byte[] imageBuffer = new byte[WIDTH * HEIGHT * BYTE_PER_PIXEL];
+
+            // データを取得する
+            PXCMImage.ImageData data;
+            pxcmStatus sts = this.image.AcquireAccess(PXCMImage.Access.ACCESS_READ, PXCMImage.PixelFormat.PIXEL_FORMAT_RGB32, out data);
+            if (sts < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                return null;
+            }
+
+            // ピクセルデータを取得する
+            Array.Clear(imageBuffer, 0, imageBuffer.Length);
+
+            // セグメンテーション画像をバイト列に変換する
+            var info = this.image.QueryInfo();
+            var b = data.ToByteArray(0, data.pitches[0] * info.height);
+
+            for (int i=0; i<(info.height*info.width); ++i)
+            {
+                var index = i * BYTE_PER_PIXEL;
+
+                // α値が0でない場合には有効な場所として色をコピーする
+                if (b[index + 3] != 0)
+                {
+                    imageBuffer[index + 0] = b[index + 0];
+                    imageBuffer[index + 1] = b[index + 1];
+                    imageBuffer[index + 2] = b[index + 2];
+                    imageBuffer[index + 3] = 255;
+                }
+                // α値が0の場合はピクセルデータのα値を0にする
+                else
+                {
+                    imageBuffer[index + 3] = 0;
+                }
+            }
+
+            // ピクセルデータを更新する
+            // Update()で行う
+
+            // データを開放する
+            this.image.ReleaseAccess(data);
+
+            return imageBuffer;
+        }
+
+        protected override void Uninitialize()
+        {
+            FLogger.Log(LogType.Debug, "child uninitialize");
+            if (this.segmentation != null)
+            {
+                this.segmentation.Dispose();
+                this.segmentation = null;
+            }
+
+            if (this.segSession != null)
+            {
+                this.segSession.Dispose();
+                this.segSession = null;
+            }
+
+            base.Uninitialize();
+        }
+    }
+}
